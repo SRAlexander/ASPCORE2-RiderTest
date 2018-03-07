659b152b4c50674e0d32fdb0c3fac50cc97b87cb
lagoutx <scottalexander@Scotts-MacBook-Pro.local>
2018-03-07 18:44:52 +0000

    EF Core Added and configured
    EntifyFramework added with test DB
    
    Preview versions do not work as of the 20180307

--- RiderMVCTest/Startup.cs
+++ RiderMVCTest/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RiderMVCTest.DAL.Context;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace RiderMVCTest
@@ -21,6 +23,9 @@ namespace RiderMVCTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<EntityContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper();
             services.AddMvc();
             
