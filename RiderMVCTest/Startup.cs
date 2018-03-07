262aa18bedaaa861eadaa89b9a52a7788eece71f
lagoutx <scottalexander@Scotts-MacBook-Pro.local>
2018-03-07 11:43:56 +0000

    Automapper Added
    With profile configured into a shared folder

--- RiderMVCTest/Startup.cs
+++ RiderMVCTest/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Cors.Internal;
@@ -20,6 +21,7 @@ namespace RiderMVCTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
             services.AddMvc();
             
             services.AddSwaggerGen(c =>
